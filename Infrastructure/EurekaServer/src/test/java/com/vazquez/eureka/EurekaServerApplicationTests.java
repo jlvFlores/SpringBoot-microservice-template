d393954dbd06d0b69406ae56ec2d91c82fdb7e4b
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2024-06-13 08:09:09 -0600

    Setup Microservice template

---
+++ Infrastructure/EurekaServer/src/test/java/com/vazquez/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vazquez.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
